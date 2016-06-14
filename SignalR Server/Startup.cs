0e54098b3e3cc332eea290d4ad2fefbc67235c24
sunjava11 <sunjava11@gmail.com>
2016-06-14 20:00:28 +0500

    Server

---
+++ SignalR Server/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalR_Server.Startup))]
+namespace SignalR_Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
